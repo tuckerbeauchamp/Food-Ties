a658a0fb6b7be6def03956c660f33e52d3350d33
Nate Shipp <nateskate256@gmail.com>
2021-04-07 14:48:03 -0700

    added models/seeds

---
+++ seeds/seed.js
@@ -0,0 +1,21 @@
+const sequelize = require('../config/connection');
+const { User, Personality } = require('../models');
+
+const userData = require('./userData.json');
+const personalityData = require('./personalityData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  const personality = await Personality.bulkCreate(personalityData, {
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
