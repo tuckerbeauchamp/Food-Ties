9d71a12ad77f1fbf16dafbc11bf93fcf79b62a1e
Nate Shipp <nateskate256@gmail.com>
2021-04-08 10:58:51 -0700

    updates server

---
+++ server.js
@@ -0,0 +1,264 @@
+const myArray = [
+    {
+    //first 4 personalities you see when taking quiz. the first question
+    //American
+    group: 1,
+    images: [
+        {
+        // for the Personality
+        persId: "spicy",
+        //URL for choice. Do path from folder
+        image_url: "ogijerwoigjeroigjr",
+        //describes image
+        alt: "fwfwefwefwef"
+        },
+        {
+        persId: "basic",
+        image_url: "wefjwefwefwef",
+        alt: "describe"
+        },
+        {
+        persId: "homebody",
+        image_url: "wefjwefwefwef",
+        alt: "describe"
+        },
+        {
+        persId: "adventurer",
+        image_url: "wefjwefwefwef",
+        alt: "describe"
+        },
+    ]
+    },
+    {
+        //Russian
+        group: 2,
+        images: [
+            {
+            // for the Personality
+            persId: "spicy",
+            //URL for choice. Do path from folder
+            image_url: "ogijerwoigjeroigjr",
+            //describes image
+            alt: "fwfwefwefwef"
+            },
+            {
+            persId: "basic",
+            image_url: "wefjwefwefwef",
+            alt: "describe"
+            },
+            {
+            persId: "homebody",
+            image_url: "wefjwefwefwef",
+            alt: "describe"
+            },
+            {
+            persId: "adventurer",
+            image_url: "wefjwefwefwef",
+            alt: "describe"
+            },
+        ]
+    },
+    {   
+        //Japan
+        group: 3,
+        images: [
+            {
+            // for the Personality
+            persId: "spicy",
+            //URL for choice. Do path from folder
+            image_url: "ogijerwoigjeroigjr",
+            //describes image
+            alt: "fwfwefwefwef"
+            },
+            {
+            persId: "basic",
+            image_url: "wefjwefwefwef",
+            alt: "describe"
+            },
+            {
+            persId: "homebody",
+            image_url: "wefjwefwefwef",
+            alt: "describe"
+            },
+            {
+            persId: "adventurer",
+            image_url: "wefjwefwefwef",
+            alt: "describe"
+            },
+        ]
+    },
+    {
+        //Italian
+        group: 4,
+        images: [
+            {
+            // for the Personality
+            persId: "spicy",
+            //URL for choice. Do path from folder
+            image_url: "ogijerwoigjeroigjr",
+            //describes image
+            alt: "fwfwefwefwef"
+            },
+            {
+            persId: "basic",
+            image_url: "wefjwefwefwef",
+            alt: "describe"
+            },
+            {
+            persId: "homebody",
+            image_url: "wefjwefwefwef",
+            alt: "describe"
+            },
+            {
+            persId: "adventurer",
+            image_url: "wefjwefwefwef",
+            alt: "describe"
+            },
+        ]
+    },
+    {   
+        //Thailand
+        group: 5,
+        images: [
+            {
+            // for the Personality
+            persId: "spicy",
+            //URL for choice. Do path from folder
+            image_url: "ogijerwoigjeroigjr",
+            //describes image
+            alt: "fwfwefwefwef"
+            },
+            {
+            persId: "basic",
+            image_url: "wefjwefwefwef",
+            alt: "describe"
+            },
+            {
+            persId: "homebody",
+            image_url: "wefjwefwefwef",
+            alt: "describe"
+            },
+            {
+            persId: "adventurer",
+            image_url: "wefjwefwefwef",
+            alt: "describe"
+            },
+        ]
+    },
+    {   
+        //Morrocan
+        group: 6,
+        images: [
+            {
+            // for the Personality
+            persId: "spicy",
+            //URL for choice. Do path from folder
+            image_url: "ogijerwoigjeroigjr",
+            //describes image
+            alt: "fwfwefwefwef"
+            },
+            {
+            persId: "basic",
+            image_url: "wefjwefwefwef",
+            alt: "describe"
+            },
+            {
+            persId: "homebody",
+            image_url: "wefjwefwefwef",
+            alt: "describe"
+            },
+            {
+            persId: "adventurer",
+            image_url: "wefjwefwefwef",
+            alt: "describe"
+            },
+        ]
+    },
+    {   
+        //Poland
+        group: 7,
+        images: [
+            {
+            // for the Personality
+            persId: "spicy",
+            //URL for choice. Do path from folder
+            image_url: "ogijerwoigjeroigjr",
+            //describes image
+            alt: "fwfwefwefwef"
+            },
+            {
+            persId: "basic",
+            image_url: "wefjwefwefwef",
+            alt: "describe"
+            },
+            {
+            persId: "homebody",
+            image_url: "wefjwefwefwef",
+            alt: "describe"
+            },
+            {
+            persId: "adventurer",
+            image_url: "wefjwefwefwef",
+            alt: "describe"
+            },
+        ]
+    },
+    {   
+        //Israeli
+        group: 8,
+        images: [
+            {
+            // for the Personality
+            persId: "spicy",
+            //URL for choice. Do path from folder
+            image_url: "ogijerwoigjeroigjr",
+            //describes image
+            alt: "fwfwefwefwef"
+            },
+            {
+            persId: "basic",
+            image_url: "wefjwefwefwef",
+            alt: "describe"
+            },
+            {
+            persId: "homebody",
+            image_url: "wefjwefwefwef",
+            alt: "describe"
+            },
+            {
+            persId: "adventurer",
+            image_url: "wefjwefwefwef",
+            alt: "describe"
+            },
+        ]
+    },
+    {   
+        //Indian
+        group: 9,
+        images: [
+            {
+            // for the Personality
+            persId: "spicy",
+            //URL for choice. Do path from folder
+            image_url: "ogijerwoigjeroigjr",
+            //describes image
+            alt: "fwfwefwefwef"
+            },
+            {
+            persId: "basic",
+            image_url: "wefjwefwefwef",
+            alt: "describe"
+            },
+            {
+            persId: "homebody",
+            image_url: "wefjwefwefwef",
+            alt: "describe"
+            },
+            {
+            persId: "adventurer",
+            image_url: "wefjwefwefwef",
+            alt: "describe"
+            },
+        ]
+    },
+]
